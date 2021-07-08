958f9e98f469f8aefc2e137ff2c608cb01e1d691
Joris Cramwinckel <joris.cramwinckel@ortec-finance.com>
2021-07-08 22:05:44 +0200

    cleared pages for fresh html build

--- ITR/data/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This module contains classes that create connections to data providers.
-"""
-
-from .data_provider import DataProvider
-from .csv import CSVProvider
-from .excel import ExcelProvider
-
